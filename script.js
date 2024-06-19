6e183766d239f96a49f1d73136c255a7577edb0b
Jasphin Vijay J <132876262+JasphinVijayJ@users.noreply.github.com>
2024-06-20 00:35:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed= new Typed(".text-1", {
+    strings: ["", "My Portfolio", ""],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+    });
+    
+var typed= new Typed(".text-2", {
+strings: ["College Student", "", "Fresher", ""],
+typeSpeed: 100,
+backSpeed: 100,
+backDelay: 1000,
+loop: true
+});
